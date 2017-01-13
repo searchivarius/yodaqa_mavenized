49686a4c53e294c0f57a3d2c71f1dcbe5540019a
searchivarius <leo@boytsov.info>
2017-01-12 20:04:13 -0500

    Let's see how the public endpoint works

--- src/main/java/cz/brmlab/yodaqa/provider/rdf/DBpediaLookup.java
+++ src/main/java/cz/brmlab/yodaqa/provider/rdf/DBpediaLookup.java
@@ -6,7 +6,8 @@ public abstract class DBpediaLookup extends CachedJenaLookup {
 	public DBpediaLookup() {
 		/* Replace the first URL below with http://dbpedia.org/sparql
 		 * to use the public DBpedia SPARQL endpoint. */
-		super("http://dbpedia.ailao.eu:3030/dbpedia/query",
+		//super("http://dbpedia.ailao.eu:3030/dbpedia/query",
+		super("http://dbpedia.org/sparql",
 			"PREFIX : <http://dbpedia.org/resource/>\n" +
 			"PREFIX dbpedia2: <http://dbpedia.org/property/>\n" +
 			"PREFIX dbpedia: <http://dbpedia.org/>\n" +
